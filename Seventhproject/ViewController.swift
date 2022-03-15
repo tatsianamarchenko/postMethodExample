342fd5a8fda0092349fe486df1d5cb10062cd81f
Tatsiana <tanya3308225@mail.ru>
2022-03-15 03:11:23 +0300

    Initial Commit

---
+++ Seventhproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seventhproject
+//
+//  Created by Tatsiana Marchanka on 15.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
