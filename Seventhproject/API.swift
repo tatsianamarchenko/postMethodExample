4d2d3af3f60fbc9f92f2feb1ddf2e463dbca3159
Tatsiana <tanya3308225@mail.ru>
2022-03-16 20:38:13 +0300

    initial commit

---
+++ Seventhproject/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  Seventhproject
+//
+//  Created by Tatsiana Marchanka on 16.03.22.
+//
+
+import Foundation
---
+++ Seventhproject/InfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InfoTableViewCell.swift
+//  Seventhproject
+//
+//  Created by Tatsiana Marchanka on 16.03.22.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
